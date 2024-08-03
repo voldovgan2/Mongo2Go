e306d2f4a653964997fc8e944d5bfff26d9a3412
Volodimir <volodimir.dovgan@hotmail.com>
2024-08-03 12:51:58 +0300

    svTest

--- MongoDownloader/Program.cs
+++ MongoDownloader/Program.cs
@@ -6,81 +6,82 @@ using System.Threading;
 using System.Threading.Tasks;
 using Spectre.Console;
 
-namespace MongoDownloader
+namespace MongoDownloader;
+
+internal static class Program
 {
-    internal static class Program
+    private static async Task<int> Main(string[] args)
     {
-        private static async Task<int> Main(string[] args)
+        try
         {
-            try
+            var toolsDirectory = GetToolsDirectory();
+
+            foreach (DirectoryInfo dir in toolsDirectory.EnumerateDirectories())
             {
-                var toolsDirectory = GetToolsDirectory();
-
-                foreach (DirectoryInfo dir in toolsDirectory.EnumerateDirectories())
-                {
-                    dir.Delete(true); 
-                }
-
-                var cancellationTokenSource = new CancellationTokenSource();
-                Console.CancelKeyPress += (_, eventArgs) =>
-                {
-                    // Try to cancel gracefully the first time, then abort the process the second time Ctrl+C is pressed
-                    eventArgs.Cancel = !cancellationTokenSource.IsCancellationRequested;
-                    cancellationTokenSource.Cancel();
-                };
-                var options = new Options();
-                var performStrip = args.All(e => e != "--no-strip") && false;
-                var binaryStripper = performStrip ? await GetBinaryStripperAsync(cancellationTokenSource.Token) : null;
-                var archiveExtractor = new ArchiveExtractor(options, binaryStripper);
-                var downloader = new MongoDbDownloader(archiveExtractor, options);
-                var strippedSize = await downloader.RunAsync(toolsDirectory, cancellationTokenSource.Token);
-                if (performStrip)
-                {
-                    AnsiConsole.WriteLine($"Saved {strippedSize:#.#} by stripping executables");
-                }
-                return 0;
+                dir.Delete(true);
             }
-            catch (Exception exception)
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, eventArgs) =>
             {
-                if (exception is not OperationCanceledException)
-                {
-                    AnsiConsole.WriteException(exception, ExceptionFormats.ShortenPaths);
-                }
-                return 1;
+                // Try to cancel gracefully the first time, then abort the process the second time Ctrl+C is pressed
+                eventArgs.Cancel = !cancellationTokenSource.IsCancellationRequested;
+                cancellationTokenSource.Cancel();
+            };
+            var options = new Options();
+            var performStrip = args.All(e => e != "--no-strip") && false;
+            var binaryStripper = performStrip ? await GetBinaryStripperAsync(cancellationTokenSource.Token) : null;
+            var archiveExtractor = new ArchiveExtractor(options, binaryStripper);
+            var downloader = new MongoDbDownloader(archiveExtractor, options);
+            var strippedSize = await downloader.RunAsync(toolsDirectory, cancellationTokenSource.Token);
+            if (performStrip)
+            {
+                AnsiConsole.WriteLine($"Saved {strippedSize:#.#} by stripping executables");
+            }
+            return 0;
+        }
+        catch (Exception exception)
+        {
+            if (exception is not OperationCanceledException)
+            {
+                AnsiConsole.WriteException(exception, ExceptionFormats.ShortenPaths);
+            }
+            return 1;
+        }
+    }
+
+    private static DirectoryInfo GetToolsDirectory()
+    {
+        for (var directory = new DirectoryInfo("."); directory != null; directory = directory.Parent)
+        {
+            var toolsDirectory = directory.GetDirectories("tools", SearchOption.TopDirectoryOnly).SingleOrDefault();
+            if (toolsDirectory?.Exists ?? false)
+            {
+                return toolsDirectory;
             }
         }
+        throw new InvalidOperationException("The tools directory was not found");
+    }
 
-        private static DirectoryInfo GetToolsDirectory()
+    private static async Task<BinaryStripper?> GetBinaryStripperAsync(CancellationToken cancellationToken)
+    {
+        try
         {
-            for (var directory = new DirectoryInfo("."); directory != null; directory = directory.Parent)
-            {
-                var toolsDirectory = directory.GetDirectories("tools", SearchOption.TopDirectoryOnly).SingleOrDefault();
-                if (toolsDirectory?.Exists ?? false)
-                {
-                    return toolsDirectory;
-                }
-            }
-            throw new InvalidOperationException("The tools directory was not found");
+            return await BinaryStripper.CreateAsync(cancellationToken);
         }
-
-        private static async Task<BinaryStripper?> GetBinaryStripperAsync(CancellationToken cancellationToken)
+        catch (FileNotFoundException exception)
         {
-            try
-            {
-                return await BinaryStripper.CreateAsync(cancellationToken);
-            }
-            catch (FileNotFoundException exception)
-            {
-                string installCommand;
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                    installCommand = "brew install llvm";
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                    installCommand = "scoop install llvm";
-                else
-                    installCommand = "apt-get install llvm";
+            string installCommand;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                installCommand = "brew install llvm";
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                installCommand = "scoop install llvm";
+            else
+                installCommand = "apt-get install llvm";
 
-                throw new Exception($"{exception.Message} Either install llvm with `{installCommand}` or run MongoDownloader with the --no-strip option to skip binary stripping.", exception);
-            }
+            throw new Exception($"{exception.Message} Either install llvm with `{installCommand}` or run MongoDownloader with the --no-strip option to skip binary stripping.", exception);
         }
     }
 }
+
+
